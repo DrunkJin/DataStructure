b3afd45f4568490567b4214c57654b86fc868a81
DrunkJin <ckae930413@gmail.com>
2022-06-14 21:41:27 +0900

    220614_12번까지

--- Chapter2_순환/exercise.c
+++ Chapter2_순환/exercise.c
@@ -27,6 +27,41 @@ int recursive(int n)
     else return( 2*recursive(n-1)+1 );
 }
 
+// 09. recursive2(10) 호출시 출력내용과 반환값
+int recursive2(int n)
+{
+    printf("%d\n", n);
+    if ( n<1) return -1;
+    else return( recursive2(n-3)+1);
+}
+
+// 10. recursive3(5) 호출시 화면에 출력되는 내용
+int recursive3(int n)
+{
+    if(n !=1) recursive3(n-1);
+    printf("%d\n", n);
+}
+
+// 11. asterisk(5) 호출할때 출력되는 *의 갯수는?
+void asterisk(int i)
+{
+    if( i > 1 ) {
+        asterisk(i/2);
+        asterisk(i/2);
+    }
+    printf("*");
+}
+
+// 12. unknown()에서 "recursive" 입력하면 뭐가 출력되는가?
+// unknown()
+// {
+//     int ch;
+//     if( (ch=getchar()) != '\n')
+//         unknown();
+//     putchar(ch);
+// }
+
+
 
 void main()
 {
@@ -39,5 +74,21 @@ void main()
     int recursiveResult = recursive(n);
     printf("recursive(%d) 답 : %d\n\n", n, recursiveResult);
     
-    return 0;
+    printf("Chapter2 09번 문제 답\n");
+    n = 10;
+    int recursive2Result = recursive2(n);
+    printf("recursive2(%d) 답 : %d\n\n", n, recursive2Result);
+    
+    printf("Chapter2 10번 문제 답\n");
+    n = 5;
+    int recursive3Result = recursive3(n);
+    printf("recursive3(%d) 답 : %d\n\n", n, recursive3Result);
+
+    printf("Chapter2 11번 문제 답\n");
+    asterisk(5); // 7개. asterisk(5)에서 1개 asterisk(2) 2개에서 각각 1개씩, asterisk(1) 4개에서 각각 1개씩
+
+    printf("Chapter2 12번 문제 답\n");
+    // unknown;
+
+    return NULL;
 }
